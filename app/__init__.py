22bef5da419c1be611797e00daaad2df2c538359
Kacper <kacper.ogrodowicz02@gmail.com>
2026-06-24 18:23:42 +0200

    panel klienta i naprawiony init

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,28 @@
-from flask import Flask
-from config import Config
+from flask import Flask, redirect, url_for
 from app.extensions import db, login_manager, migrate
+from config import Config
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'Zaloguj się, aby uzyskać dostęp.'
     migrate.init_app(app, db)
+
+    from app.models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    from app.client import bp as client_bp
-    app.register_blueprint(client_bp, url_prefix='/client')
+    from app.boss import bp as boss_bp
+    app.register_blueprint(boss_bp, url_prefix='/boss')
 
     from app.reception import bp as reception_bp
     app.register_blueprint(reception_bp, url_prefix='/reception')
@@ -22,12 +30,24 @@ def create_app(config_class=Config):
     from app.mechanic import bp as mechanic_bp
     app.register_blueprint(mechanic_bp, url_prefix='/mechanic')
 
-    from app.boss import bp as boss_bp
-    app.register_blueprint(boss_bp, url_prefix='/boss')
+    from app.client import bp as client_bp
+    app.register_blueprint(client_bp, url_prefix='/client')
 
     @app.route('/')
     def index():
-        from flask import render_template
-        return render_template('index.html')
+        from flask_login import current_user
+        if current_user.is_authenticated:
+            if current_user.role == 'szef':
+                return redirect(url_for('boss.employees'))
+            elif current_user.role == 'recepcja':
+                return redirect(url_for('reception.orders'))
+            elif current_user.role == 'mechanik':
+                return redirect(url_for('mechanic.my_orders'))
+            else:
+                return redirect(url_for('client.my_garage'))
+        return redirect(url_for('auth.login'))
+
+    with app.app_context():
+        db.create_all()
 
     return app
